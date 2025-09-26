1a77268d867c81da7405a1dcf21801a081ebc38a
Bukola <bukolafaduagba@gmail.com>
2025-09-26 19:39:18 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Search from './components/Search'
 import Spinner from './components/Spinner';
 import { MovieCard } from './components/MovieCard';
 import { useDebounce } from 'react-use';
-import { getTrendingMovies,updateSearchCount } from './AppWrite';
+import { getTrendingMovies,updateSearchCount } from './appWrite';
 import { Filters } from './components/Filter';
 
 
